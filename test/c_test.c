7180a38f8c8cb7b91b5858dd3b66a2843cf15466
RKE <5091975+cardcomm@users.noreply.github.com>
2026-08-27 21:53:14 -0500

    Remove development test files from completed port

--- test/c_test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <conio.h>
-#include <apple2enh.h>
-
-int main(void)
-{
-    return 0;
-}
--- test/home_test.s
+++
@@ -1,7 +0,0 @@
-.setcpu "65C02"
-
-.segment "CODE"
-
-start:
-        jsr $FC58
-        rts
